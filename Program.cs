026296f7a735d1be6033730ef5db90097b0add23
Enzzo Lima Alves <enzzolimaalves2013@gmail.com>
2020-11-18 11:25:04 -0300

    Exercicios até o 2°

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace exercicios_repeticao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //Exercicio 1
+            // Console.WriteLine("Digite uma nota de 0 a 10: ");
+            // float nota = float.Parse(Console.ReadLine());
+            // if( nota > 10)
+            // {
+            //     Console.WriteLine("Número Inválido! Digite Novamente:");
+            // }
+                
+          //Exercicio 2 
+            //    int i = 0;
+            //    string senha , usuario;
+            //    while(i < 1)
+            //    {
+            //         Console.WriteLine("Digite o nome de seu usuário: ");
+            //         usuario = Console.ReadLine();
+            //         Console.WriteLine("Digite a sua senha: ");
+            //         senha = Console.ReadLine();
+            //         if(senha != usuario){
+            //             i++;
+            //         }else{
+            //             Console.WriteLine("Senha precisa ser diferente de usuário!");
+            //         }
+            //     } 
+        }
+    }
+}
